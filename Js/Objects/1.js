bead829ae384aedd5590ea0c1fc83bce70307bb7
Isaac Mwangi <108072119+Isaacmwangi@users.noreply.github.com>
2024-06-26 11:46:38 +0300

    Delete Js directory

--- Js/Objects/1.js
+++
@@ -1,7 +0,0 @@
-var student = {
-    name: "David Rayy",
-    sclass: "VI",
-    rollno: 12
-};
-
-console.log(Object.keys(student).join(","));//I have used.join to to avoid showing in a table format and instead show as a string
